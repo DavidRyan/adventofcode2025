6cf71d84cea37b3be71508d99f923a22ed2049d5
David Ryan <dryan@mbp.local>
2025-12-02 15:49:09 -0600

    day1 part 2

--- src/day1.kt
+++ src/day1.kt
@@ -1,34 +1,50 @@
 import java.io.File
 
-enum class Direction {
-    LEFT,
-    RIGHT
-}
-
 data class Step(val mod: (Int, Int) -> Int, val clicks: Int)
-
+data class Step2(val mod: (Int, Int) -> Pair<Int, Int>, val clicks: Int, val positive: Boolean)
 
 fun main(args: Array<String>) {
-    var ticker: Int = 50
-    var zeros = 0
+    val input = File(args[0]).readLines()
+    part2(input)
+    part1(input)
+}
+
+fun part2(input: List<String>) : Int {
+    val left: (Int, Int) -> Pair<Int, Int> = { c, t ->
+        val newTicker = (((t - c) % 100) + 100) % 100
+        newTicker to (c - t+ 99) / 100
+    }
+    val right : (Int, Int) -> Pair<Int, Int> = { c, t ->
+        val newTicker = (((c + t) % 100) + 100) % 100
+        newTicker to (newTicker + c) / 100
+    }
+   return input
+        .map {
+            val mod = if (it.substring(0, 1) == "L") left else right
+            Step2(mod,it.substring(1).toInt(), it.substring(0, 1) == "R")
+        }
+        .fold(50 to 0) { (ticker, rotations), step ->
+            val (newTicker, newRotation) = step.mod(step.clicks, ticker)
+            (newTicker to rotations + newRotation)
+        }.second
+}
+
+fun part1(input: List<String>) : Int {
     val left: (Int, Int) -> Int = { c, t ->
         (((c + t) % 100) + 100) % 100
     }
     val right : (Int, Int) -> Int = { c, t ->
         (((t- c) % 100) + 100) % 100
     }
-    File(args[0]).readLines()
+    return input
         .map {
             val mod = if (it.substring(0, 1) == "L") left else right
             Step(mod,it.substring(1).toInt())
         }
-        .forEach {
-            ticker = it.mod(it.clicks, ticker)
-            if (ticker == 0) zeros++
-        }
-    println(zeros)
+        .fold(50 to 0) { (ticker, zeros), step ->
+            val newTicker = step.mod(step.clicks, ticker)
+            val newZero = zeros + if (newTicker == 0) 1 else 0
+            (newTicker to newZero)
+        }.second
 }
-
-
-
 
